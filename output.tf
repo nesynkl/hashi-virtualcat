9fc956e7dbd33850c1f8fb643d40a4e40f78fe39
Rodolfo Castelo <rodolfo.castelomendez@hashicorp.com>
2024-07-09 16:29:23 +0200

    outputs

--- output.tf
+++ output.tf
@@ -1,18 +1,14 @@
 # Outputs file
-output "first_create_key" {
-  value     = "printf \"${tls_private_key.hashicat.private_key_pem}\" > ./private_key.pem"
+output "connection" {
+  value     = <<SSHCONFIG
+  printf \"${tls_private_key.hashicat.private_key_openssh}\" > ./private_key.pem"
+  chmod 600 ./private_key.pem
+  ssh -i ./private_key.pem ubuntu@${aws_eip.hashicat.public_dns}
+  SSHCONFIG
   sensitive = true
 }
 
-output "second_permissions" {
-  value = "chmod 600 ./private_key.pem"
-}
-
-output "third_ssh_command" {
-  value = "ssh -i ./private_key.pem ubuntu@${aws_eip.hashicat.public_dns}"
-}
-
 output "private_key" {
-  value     = tls_private_key.hashicat.private_key_pem
+  value     = tls_private_key.hashicat.private_key_openssh
   sensitive = true
 }
