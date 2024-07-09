004dbe60ca46afa9366d2f211864693cfe030862
Rodolfo Castelo <rodolfo.castelomendez@hashicorp.com>
2024-07-09 16:18:27 +0200

    update outputs

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 # Outputs file
-output "1_create_key" {
+output "first_create_key" {
   value     = "echo -e \"${tls_private_key.hashicat.private_key_pem}\" > ./private_key.pem"
   sensitive = true
 }
 
-output "2_permissions" {
+output "second_permissions" {
   value = "chmod 600 ./private_key.pem"
 }
 
-output "3_ssh_command" {
+output "third_ssh_command" {
   value = "ssh -i ./private_key.pem ubuntu@${aws_eip.hashicat.public_dns}"
 }
 
