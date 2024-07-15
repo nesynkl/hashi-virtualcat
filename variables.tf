fc2879eb90dbd480a502e44c5d6b077122c3d50a
nesynkl <nesy.nkokolo@hashicorp.com>
2024-07-15 17:07:55 +0200

    Update instance type.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "instance_type" {
   description = "Specifies the AWS instance type."
   type        = string
-  default     = "t2.micro"
+  default     = "t2.medium"
 }
 
 variable "region" {
